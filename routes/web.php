e40fef51bb9360eeb388d171fa7b96df09f3b030
Qk29 <qkbn2001@gmail.com>
2025-03-20 14:35:41 +0700

    practice

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $users1 = DB::table('users')->get();
+ 
+
+    $users = DB::connection('mysql2_connection')->table('users')->get();
+    dd($users,$users1);
 });
